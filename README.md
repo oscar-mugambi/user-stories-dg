c231e4d8666c2e5fe67280c3bbef10211b274fbd
oscar mugambi <oscarmugambi@gmail.com>
2023-01-12 19:29:29 +0300

    added permisions

---
+++ README.md
@@ -0,0 +1,2 @@
+Currently at 6hr:01min
+About to install jwt-decode so we can work on permissions
--- src/features/auth/Welcome.jsx
+++ src/features/auth/Welcome.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
 
 const Welcome = () => {
+  const { username, isManager, isAdmin } = useAuth()
+
   const date = new Date()
   const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(
     date
@@ -11,7 +14,7 @@ const Welcome = () => {
     <section className='welcome'>
       <p>{today}</p>
 
-      <h1>Welcome!</h1>
+      <h1>Welcome {username}</h1>
 
       <p>
         <Link to='/dash/notes'>View techNotes</Link>
@@ -21,13 +24,17 @@ const Welcome = () => {
         <Link to='/dash/notes/new'>Add New techNote</Link>
       </p>
 
-      <p>
-        <Link to='/dash/users'>View Users</Link>
-      </p>
+      {(isManager || isAdmin) && (
+        <p>
+          <Link to='/dash/users'>View User Settings</Link>
+        </p>
+      )}
 
-      <p>
-        <Link to='/dash/users/new'>Add New User</Link>
-      </p>
+      {(isManager || isAdmin) && (
+        <p>
+          <Link to='/dash/users/new'>Add New User</Link>
+        </p>
+      )}
     </section>
   )
 
